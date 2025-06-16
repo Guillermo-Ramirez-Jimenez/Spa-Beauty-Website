b442b331398b5b336519670ab542d948e59efa4c
Chache <guillermo.ramirez.jimenez.0@gmail.com>
2025-06-16 20:26:55 +0200

    Vite base changed again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Spa-Beauty-Website/", // Usa rutas relativas en los enlaces generados en el HTML
+  base: "https://guillermo-ramirez-jimenez.github.io/Spa-Beauty-Website/", // Usa rutas relativas en los enlaces generados en el HTML
 });
 
 
